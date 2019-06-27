cda5c8144571883d181a4c059f638b4e24007dec
Motoaki Sumi <msumi@mba2019.hbs.edu>
2019-06-27 05:37:20 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -3,7 +3,7 @@ def sort_array_asc(array)
   return array.sort
 end
 
-#def sort_array_desc(array)
-#  array.sort do |a, b|
-#  a <=> b
-#end
+def sort_array_desc(array)
+  array.sort do |a, b|
+  a <=> b
+end
