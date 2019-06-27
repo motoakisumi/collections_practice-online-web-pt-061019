f9e5f571b1b0c583853b29b0cb1e0f33bc21705c
Motoaki Sumi <msumi@mba2019.hbs.edu>
2019-06-27 05:42:54 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -9,11 +9,11 @@ end
 
 def sort_array_char_count(array)
   array.sort do |a, b|
-  if a.length == b.length
-    0
-  elsif a.length < b.length
-    -1
-  elsif a.length > b.length
-    1
+    if a.length == b.length
+      0
+    elsif a.length < b.length
+      -1
+    elsif a.length > b.length
+      1
   end
 end
