d42e41ab9efb7fffb45a7d90be342f81f12bac08
Motoaki Sumi <msumi@mba2019.hbs.edu>
2019-06-27 06:23:15 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,59 @@
 
+def sort_array_asc(array)
+  return array.sort
+end
+
+def sort_array_desc(array)
+  return array.sort.reverse
+end
+
+def sort_array_char_count(array)
+  array.sort do |a, b|
+    if a.length == b.length
+      0
+    elsif a.length < b.length
+      -1
+    elsif a.length > b.length
+      1
+    end
+  end
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  return array
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  array.collect do |word|
+    word = word.split("")
+    word[2] = "$"
+    word.join
+  end
+end
+
+def find_a(array)
+  array.select{|word| word.start_with?("a")}
+end
+
+def sum_array(array)
+  array.inject{|sum, number| sum + number}
+end
+
+def add_s(array)
+  counter = 0
+  array.collect do |word|
+    if counter !=1
+      counter += 1
+      word = word + "s"
+    else
+      counter +=1
+      word = word
+    end
+  end
+end
+
